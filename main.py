ca9aa455bf2122201d511f790aadfd9d765b152b
nieves <930112ab@gmail.com>
2025-03-02 16:19:01 +0800

    learn sprite

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import pygame
 
 FPS = 60
-WHITE = (255, 255, 255)
+
 WIDTH = 500
 HEIGHT = 600
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 
 # 遊戲初始化 & 創建視窗
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("space survival battle")
 clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        
+    def update(self):
+        self.rect.x += 2
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+        
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 running = True
 
@@ -21,9 +42,11 @@ while running:
             running = False
     
     # 更新遊戲
+    all_sprites.update()
     
     # 畫面顯示
     screen.fill(WHITE)
+    all_sprites.draw(screen)
     pygame.display.update()
     
 pygame.quit()
